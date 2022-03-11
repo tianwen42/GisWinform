542aa8f36ee9c6dbb1cd929bdba2e62a2e83155a
YangLin <2274941409@qq.com>
2022-03-11 16:14:06 +0800

    no message

--- gis_1/Form1.Designer.cs
+++ gis_1/Form1.Designer.cs
@@ -31,7 +31,6 @@ namespace gis_1
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
             this.panel4 = new System.Windows.Forms.Panel();
             this.panel5 = new System.Windows.Forms.Panel();
@@ -42,6 +41,8 @@ namespace gis_1
             this.axTOCControl1 = new ESRI.ArcGIS.Controls.AxTOCControl();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.axToolbarControl1 = new ESRI.ArcGIS.Controls.AxToolbarControl();
+            this.btn_openmxd = new System.Windows.Forms.Button();
+            this.btn_openshp = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel3.SuspendLayout();
             this.panel5.SuspendLayout();
@@ -61,21 +62,13 @@ namespace gis_1
             this.panel1.Size = new System.Drawing.Size(879, 100);
             this.panel1.TabIndex = 5;
             // 
-            // panel2
-            // 
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(0, 463);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(879, 100);
-            this.panel2.TabIndex = 6;
-            // 
             // panel3
             // 
             this.panel3.Controls.Add(this.axTOCControl1);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel3.Location = new System.Drawing.Point(0, 100);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(200, 363);
+            this.panel3.Size = new System.Drawing.Size(200, 463);
             this.panel3.TabIndex = 7;
             // 
             // panel4
@@ -83,16 +76,17 @@ namespace gis_1
             this.panel4.Dock = System.Windows.Forms.DockStyle.Right;
             this.panel4.Location = new System.Drawing.Point(679, 100);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(200, 363);
+            this.panel4.Size = new System.Drawing.Size(200, 463);
             this.panel4.TabIndex = 8;
             // 
             // panel5
             // 
             this.panel5.Controls.Add(this.axMapControl1);
+            this.panel5.Controls.Add(this.axToolbarControl1);
             this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel5.Location = new System.Drawing.Point(200, 100);
             this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(479, 363);
+            this.panel5.Size = new System.Drawing.Size(479, 463);
             this.panel5.TabIndex = 9;
             // 
             // tabControl1
@@ -109,7 +103,8 @@ namespace gis_1
             // 
             // tabpage1
             // 
-            this.tabpage1.Controls.Add(this.axToolbarControl1);
+            this.tabpage1.Controls.Add(this.btn_openshp);
+            this.tabpage1.Controls.Add(this.btn_openmxd);
             this.tabpage1.Location = new System.Drawing.Point(4, 25);
             this.tabpage1.Name = "tabpage1";
             this.tabpage1.Padding = new System.Windows.Forms.Padding(3);
@@ -131,11 +126,12 @@ namespace gis_1
             // axMapControl1
             // 
             this.axMapControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.axMapControl1.Location = new System.Drawing.Point(0, 0);
+            this.axMapControl1.Location = new System.Drawing.Point(0, 28);
             this.axMapControl1.Name = "axMapControl1";
             this.axMapControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axMapControl1.OcxState")));
-            this.axMapControl1.Size = new System.Drawing.Size(479, 363);
+            this.axMapControl1.Size = new System.Drawing.Size(479, 435);
             this.axMapControl1.TabIndex = 0;
+            this.axMapControl1.OnMouseDown += new ESRI.ArcGIS.Controls.IMapControlEvents2_Ax_OnMouseDownEventHandler(this.axMapControl1_OnMouseDown);
             // 
             // axTOCControl1
             // 
@@ -143,8 +139,9 @@ namespace gis_1
             this.axTOCControl1.Location = new System.Drawing.Point(0, 0);
             this.axTOCControl1.Name = "axTOCControl1";
             this.axTOCControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axTOCControl1.OcxState")));
-            this.axTOCControl1.Size = new System.Drawing.Size(200, 363);
+            this.axTOCControl1.Size = new System.Drawing.Size(200, 463);
             this.axTOCControl1.TabIndex = 0;
+            this.axTOCControl1.OnMouseDown += new ESRI.ArcGIS.Controls.ITOCControlEvents_Ax_OnMouseDownEventHandler(this.axTOCControl1_OnMouseDown);
             // 
             // tabPage3
             // 
@@ -158,12 +155,33 @@ namespace gis_1
             // 
             // axToolbarControl1
             // 
-            this.axToolbarControl1.Location = new System.Drawing.Point(3, 6);
+            this.axToolbarControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.axToolbarControl1.Location = new System.Drawing.Point(0, 0);
             this.axToolbarControl1.Name = "axToolbarControl1";
             this.axToolbarControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axToolbarControl1.OcxState")));
-            this.axToolbarControl1.Size = new System.Drawing.Size(860, 28);
+            this.axToolbarControl1.Size = new System.Drawing.Size(479, 28);
             this.axToolbarControl1.TabIndex = 0;
             this.axToolbarControl1.OnMouseDown += new ESRI.ArcGIS.Controls.IToolbarControlEvents_Ax_OnMouseDownEventHandler(this.axToolbarControl1_OnMouseDown);
+            // 
+            // btn_openmxd
+            // 
+            this.btn_openmxd.Location = new System.Drawing.Point(8, 6);
+            this.btn_openmxd.Name = "btn_openmxd";
+            this.btn_openmxd.Size = new System.Drawing.Size(91, 59);
+            this.btn_openmxd.TabIndex = 1;
+            this.btn_openmxd.Text = "加载mxd";
+            this.btn_openmxd.UseVisualStyleBackColor = true;
+            this.btn_openmxd.Click += new System.EventHandler(this.btn_openmxd_Click);
+            // 
+            // btn_openshp
+            // 
+            this.btn_openshp.Location = new System.Drawing.Point(106, 7);
+            this.btn_openshp.Name = "btn_openshp";
+            this.btn_openshp.Size = new System.Drawing.Size(90, 58);
+            this.btn_openshp.TabIndex = 2;
+            this.btn_openshp.Text = "加载shp";
+            this.btn_openshp.UseVisualStyleBackColor = true;
+            this.btn_openshp.Click += new System.EventHandler(this.btn_openshp_Click);
             // 
             // Form1
             // 
@@ -173,7 +191,6 @@ namespace gis_1
             this.Controls.Add(this.panel5);
             this.Controls.Add(this.panel4);
             this.Controls.Add(this.panel3);
-            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -196,13 +213,14 @@ namespace gis_1
         private System.Windows.Forms.TabPage tabpage1;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.TabPage tabPage3;
-        private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel3;
         private ESRI.ArcGIS.Controls.AxTOCControl axTOCControl1;
         private System.Windows.Forms.Panel panel4;
         private System.Windows.Forms.Panel panel5;
         private ESRI.ArcGIS.Controls.AxMapControl axMapControl1;
         private ESRI.ArcGIS.Controls.AxToolbarControl axToolbarControl1;
+        private System.Windows.Forms.Button btn_openmxd;
+        private System.Windows.Forms.Button btn_openshp;
     }
 }
 
--- gis_1/Form1.cs
+++ gis_1/Form1.cs
@@ -8,6 +8,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+using ESRI.ArcGIS.esriSystem;
+using ESRI.ArcGIS.SystemUI;
+using ESRI.ArcGIS.Controls;
+using ESRI.ArcGIS.Carto;
+
+
+
 namespace gis_1
 {
     public partial class Form1 : Form
@@ -21,5 +29,83 @@ namespace gis_1
         {
 
         }
-    }
+
+        private void axMapControl1_OnMouseDown(object sender, ESRI.ArcGIS.Controls.IMapControlEvents2_OnMouseDownEvent e)
+        {
+
+        }
+
+        private void axTOCControl1_OnMouseDown(object sender, ESRI.ArcGIS.Controls.ITOCControlEvents_OnMouseDownEvent e)
+        {
+
+        }
+
+        private void btn_openmxd_Click(object sender, EventArgs e)
+        {
+            /// <summary>
+            /// 加载Mxd
+            /// </summary>
+            /// <param name="sender"></param>
+            /// <param name="e"></param>
+
+            OpenFileDialog ofd = new OpenFileDialog()   //实例化打开文件功能
+            {
+                Title = "打开Mxd文档",
+                Filter = "打开Mxd文档(*.mxd)|*.mxd"
+            };
+            if (ofd.ShowDialog() == DialogResult.OK)  //打开对话框，并选择OK
+            {
+                string filename = ofd.FileName;  //打开对话框，并选择OK
+                ofd.RestoreDirectory = true;     //打开对话框，并选择OK
+                axMapControl1.LoadMxFile(filename);
+                axMapControl1.Refresh();
+                axTOCControl1.Refresh();
+            }
+
+        }
+
+
+        /// <summary>
+        /// 加载Shp文件
+        /// </summary>
+        /// <param name="sender"></param>
+        ///// <param name="e"></param>
+        private void btn_openshp_Click(object sender, EventArgs e)
+        {
+            //    //打开选取文件的对话框
+            //    OpenFileDialog ofd = new OpenFileDialog()   //实例化打开文件功能
+            //    {
+            //        Title = "打开shp图层",
+            //        Filter = "打开shp图层(*.shp)|*.shp"
+            //    };
+            //    ofd.ShowDialog();
+            //    ///已经获取到shp文件的路径
+            //    ///
+            //    string fullFileName = ofd.FileName;
+            //    if (fullFileName == "") return;
+
+            //    ///提前路径和文件名
+            //    int index = fullFileName.LastIndexOf("\\");
+            //    string path = fullFileName.Substring(0, index);  //路径
+            //    string filename = fullFileName.Substring(index + 1);  //文件名
+
+            //    ///创建工作空间
+            //    IWorkspaceFactory pWorkspaceFactory;
+            //    pWorkspaceFactory = new ShapefileWorkspaceFactory();
+            //    IFeatureWorkspace pFeatureWorkspace = pWorkspaceFactory.OpenFromFile(path, 0) as IFeatureWorkspace;
+
+            //    //提前本工作空间下的要素类
+            //    IFeatureClass pFeatureClass = pFeatureWorkspace.OpenFeatureClass(filename);
+
+            //    //将要素类转化成要素图层  为地图图层做准备
+            //    IFeatureLayer pFeatureLayer = new FeatureLayer();  //实例化一个要素图层
+            //    pFeatureLayer.FeatureClass = pFeatureClass;
+            //    pFeatureLayer.Name = "我喜欢的名字";
+
+            //    //将图层加载到地图窗口
+            //    axMapControl1.Map.AddLayer(pFeatureLayer as ILayer);
+            //    axMapControl1.Refresh();
+            //    axTOCControl1.Refresh();
+            }
+        }
 }
